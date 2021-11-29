e49b88091b58e3fc89a044719f603eb9873076b2
AnaKaprielian <anakaprielian@gmail.com>
2021-11-29 12:10:38 -0300

    covid

--- PD3/src/test/java/uy/edu/ucu/aed2/TestRedElectrica/TestCases.java
+++ PD3/src/test/java/uy/edu/ucu/aed2/TestRedElectrica/TestCases.java
@@ -3,8 +3,10 @@ package uy.edu.ucu.aed2.TestRedElectrica;
 import org.junit.Before;
 import org.junit.Test;
 
+import uy.edu.ucu.aed2.TAnillosContagio;
 import uy.edu.ucu.aed2.TArista;
 import uy.edu.ucu.aed2.TAristas;
+import uy.edu.ucu.aed2.TGrafoContagios;
 import uy.edu.ucu.aed2.TGrafoDirigido;
 import uy.edu.ucu.aed2.TGrafoNoDirigido;
 import uy.edu.ucu.aed2.TGrafoRedElectrica;
@@ -25,6 +27,7 @@ public class TestCases {
     TAristas as;
     TGrafoRedElectrica g;
     TGrafoNoDirigido g2;
+    TGrafoContagios gContagios;
 
     @Before
     public void setUp(){
@@ -92,11 +95,62 @@ public class TestCases {
         boolean camino = g2.hayCamino("1", "4");
         boolean noCamino = g2.hayCamino("1", "5");
         
-        
-        
         assertTrue(camino);
         assertFalse(noCamino);
     }
+
+    @Test
+    public void anillosContagio(){
+        TArista a12 = new TArista("1", "2", 2);
+        TArista a13 = new TArista("1", "3", 5);
+        TArista a34 = new TArista("3", "4", 6);
+        TArista a24 = new TArista("2", "4", 1);
+        TArista a32 = new TArista("3", "2", 3);
+        TArista a14 = new TArista("1", "4", 4);
+        TArista a25 = new TArista("2", "5", 7);
+        TArista a45 = new TArista("4", "5", 8);
+        
+        as = new TAristas();
+        as.add(a12);
+        as.add(a13);
+        as.add(a34);
+        as.add(a24);
+        as.add(a32);
+        as.add(a14);
+        as.add(a25);
+        as.add(a45);
+        
+        gContagios = new TGrafoContagios(vertices, as);
+        TAnillosContagio aContagio = gContagios.anillosDeProbablesContagiados("1", 1);
+        int expected = aContagio.get(1).size();
+       
+        assertEquals(expected, 3);
+    }
+
+    @Test
+    public void anillosContagioDeCero(){
+        TArista a12 = new TArista("1", "2", 2);
+        TArista a13 = new TArista("1", "3", 5);
+        TArista a34 = new TArista("3", "4", 6);
+        TArista a24 = new TArista("2", "4", 1);
+        TArista a32 = new TArista("3", "2", 3);
+        TArista a45 = new TArista("4", "5", 8);
+        
+        as = new TAristas();
+        as.add(a12);
+        as.add(a13);
+        as.add(a34);
+        as.add(a24);
+        as.add(a32);
+        as.add(a45);
+        
+        gContagios = new TGrafoContagios(vertices, as);
+        TAnillosContagio aContagio = gContagios.anillosDeProbablesContagiados("1", 0);
+        int expected = aContagio.size() - 1;
+       
+        assertEquals(expected, 3);
+    }
+
 
 
 
